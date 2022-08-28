0aadc5d40ff62469d82f4ddf75316dfc0c534f62
NedalAlQaisi <n.alqaisi221999@gmail.com>
2022-08-28 16:26:54 +0300

    edit link
    edit link

--- Project_4/assets/js/main.js
+++ Project_4/assets/js/main.js
@@ -34,3 +34,44 @@ function carousel() {
     x[myIndex - 1].style.display = "block";
     setTimeout(carousel, 2500);
 }
+
+function ShowThisToolBarView(el){
+    $('.MainView').hide()
+    $('#OrderTypeOpensHere').hide()
+    $('#OrderTypeOpensHere iframe').attr('src','')
+    $('.AddOrdersOpened').click()
+    $(el.attr('rel')).show()
+
+    $('.MainView').scrollTop(0);
+    $('.toolbar button').removeClass('ActiveToolbarBTN')
+    $('.toolbar h1').removeClass('ActiveToolbarBTN')
+    el.addClass('ActiveToolbarBTN')
+}
+
+function ToggleAddOrder(el){
+    if($('.OrderTypesView').is(':visible')){
+        $('.OrderTypesView').hide()
+        el.removeClass('AddOrdersOpened')
+        if($('#Home').is(':visible')){
+            $('.toolbar button').removeClass('ActiveToolbarBTN')
+            $('#HomeBTn').addClass('ActiveToolbarBTN')
+        }else if($('#MyNot').is(':visible')){
+            $('.toolbar button').removeClass('ActiveToolbarBTN')
+            $('#MyNotBtn').addClass('ActiveToolbarBTN')
+        }else{
+            $('.toolbar button').removeClass('ActiveToolbarBTN')
+        }
+    }else{
+        $('.OrderTypesView').show()
+        el.addClass('AddOrdersOpened')
+        $('.toolbar button').removeClass('ActiveToolbarBTN')
+    }
+
+}
+
+function ShowThisToolBarView(el){
+    $('.toolbar button').removeClass('ActiveToolbarBtn')
+    el.addClass('ActiveToolbarBtn')
+    $('.MainView').hide()
+    $(el.attr('rel')).show()
+}
